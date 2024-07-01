1ea2d0421bcf4d31fe6b562204155bbd7be27d0f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-01 21:50:21 +0300

    modularized auth to support multiple auth providers

---
+++ server/src/auth-providers/google/google.auth.mjs
@@ -0,0 +1,32 @@
+import passport from "passport";
+import dotenv from "dotenv";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+dotenv.config();
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  done(null, profile);
+};
+
+passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallback));
+
+export const googleAuth = passport.authenticate("google", {
+  scope: ["email", "profile"],
+});
+
+export const googleCallback = passport.authenticate("google", {
+  failureRedirect: "/auth/google",
+  successRedirect: "/",
+  session: false,
+});
--- server/src/routes/authentication/auth.mjs
+++ server/src/routes/authentication/auth.mjs
@@ -1,40 +1,12 @@
 import express from "express";
-import passport from "passport";
-import dotenv from "dotenv";
-import { Strategy } from "passport-google-oauth20";
-dotenv.config();
+import {
+  googleAuth,
+  googleCallback,
+} from "../../auth-providers/google/google.auth.mjs";
 
 const authRouter = express.Router();
 
-const config = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-};
+authRouter.get("/google", googleAuth);
+authRouter.get("/google/callback", googleCallback);
 
-const AUTH_OPTIONS = {
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
-};
-
-const verfiyCallback = (accessToken, refreshToken, profile, done) => {
-  console.log(profile);
-  done(null, profile);
-};
-
-passport.use(new Strategy(AUTH_OPTIONS, verfiyCallback));
-
-authRouter.get(
-  "/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-
-authRouter.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/auth/google",
-    successRedirect: "/",
-    session: false,
-  })
-);
 export default authRouter;
