8aac90eb835bb4c788021a04e6770a306c78e2dd
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-16 22:06:39 +0300

    rename folder

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+
+import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../api-requests/requests";
+
 import "./navigation.styles.css";
 const NavBar = () => {
+  const { currentUser } = useContext(UserContext);
   return (
     <>
       <nav className="navbar">
@@ -14,9 +20,15 @@ const NavBar = () => {
         <Link to="write" className="nav-link">
           <Button variant="light">Write</Button>
         </Link>
-        <Link to="authentication/sign-in" className="nav-link">
-          <Button variant="dark">Sign in</Button>
-        </Link>
+        {currentUser ? (
+          <Button variant="dark" onClick={() => signOutUser()}>
+            Sign out
+          </Button>
+        ) : (
+          <Link to="authentication/sign-in" className="nav-link">
+            <Button variant="dark">Sign in</Button>
+          </Link>
+        )}
       </nav>
       <Outlet />
     </>
