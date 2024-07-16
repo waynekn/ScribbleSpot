7ad563c63cc8e93cf3270b8a2d67493bac4c81f4
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-16 22:16:02 +0300

    add router for auth

--- server/src/routes/authentication/auth.router.mjs
+++ server/src/routes/authentication/auth.router.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import {
   googleAuth,
   googleCallback,
+  signOutUser,
 } from "../../auth-providers/google/google.auth.mjs";
 
 const authRouter = express.Router();
@@ -9,9 +10,13 @@ const authRouter = express.Router();
 authRouter.get("/google/signin", (req, res, next) => {
   googleAuth(req, res, next, "signin");
 });
+
 authRouter.get("/google/signup", (req, res, next) => {
   googleAuth(req, res, next, "signup");
 });
+
 authRouter.get("/google/callback", googleCallback);
+
+authRouter.post("/logout", signOutUser);
 
 export default authRouter;
