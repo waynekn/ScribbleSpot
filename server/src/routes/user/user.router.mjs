7c3e8b3a841ff259e6ca9e94edeef3e178565021
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-16 22:17:25 +0300

    add a router for users

--- server/src/routes/user/user.router.mjs
+++ server/src/routes/user/user.router.mjs
@@ -1,3 +1,8 @@
 import express from "express";
+import { fetchUserProfile } from "./user.controller.mjs";
 
 const userRouter = express.Router();
+
+userRouter.get("/profile", fetchUserProfile);
+
+export default userRouter;
