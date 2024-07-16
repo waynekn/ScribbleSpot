e0cf9d3e511390ba65874311527e54db627abb89
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-16 22:10:31 +0300

    move api requests into their own folder

--- client/src/api-requests/requests.js
+++ client/src/api-requests/requests.js
@@ -1,7 +1,15 @@
+import axios from "axios";
 export const URL = `http://localhost:8000`;
 
 export const authenticateUser = (provider, action) => {
   window.location.href = `${URL}/auth/${provider}/${action}`;
 };
 
-export const getProfile = async () => {};
+export const getUserProfile = async () => {
+  const res = await axios.get(`${URL}/users/profile`);
+  return res.data;
+};
+
+export const signOutUser = () => {
+  axios.post(`${URL}/auth/logout`);
+};
