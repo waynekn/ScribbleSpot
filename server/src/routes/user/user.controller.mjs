b1014d5ca9993d98b2415d16efc4f8df894fbcba
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-18 02:00:49 +0300

    remove a console.log

--- server/src/routes/user/user.controller.mjs
+++ server/src/routes/user/user.controller.mjs
@@ -5,7 +5,6 @@ export const fetchUserProfile = async (req, res) => {
     const profile = await findUserProfile(req.user.displayName);
     res.status(200).json({ profile });
   } catch (error) {
-    console.error(chalk.red("Error fetching user profile: "), error);
     res.status(404).send("Profile not found");
   }
 };
