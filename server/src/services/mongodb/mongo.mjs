b366ee40dc51ffbeb91456d3eb101c92cbb26b86
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-29 19:39:27 +0300

    remove mongoose connect from server.mjs to its own file

---
+++ server/src/services/mongodb/mongo.mjs
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once("open", () => console.log("Mongo db connected"));
+
+mongoose.connection.on("error", (err) => console.warn(err));
+
+const mongoConnect = async () => {
+  await mongoose.connect(MONGO_URL);
+};
+
+export default mongoConnect;
