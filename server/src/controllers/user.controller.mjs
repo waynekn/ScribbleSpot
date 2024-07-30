df7850f63410dcdab5b913511af698a917661fc1
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-30 22:48:16 +0300

    add changing of display name functionality

--- server/src/controllers/user.controller.mjs
+++ server/src/controllers/user.controller.mjs
@@ -1,29 +1,5 @@
-import path from "node:path";
-import multer from "multer";
-import { uploadImage, getSignedImageUrl } from "../services/aws/s3.mjs";
-import { findUserProfile } from "../models/user.model.mjs";
-
-const storage = new multer.memoryStorage();
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 10000000, // 10MB
-  },
-  fileFilter: (req, file, cb) => {
-    const fileTypesRegex = /\.(jpeg|jpg|png)$/i;
-    const extName = path.extname(file.originalname);
-
-    if (fileTypesRegex.test(extName)) {
-      cb(null, true);
-    } else {
-      cb(
-        new Error(`Error: Only these types are expected: .jpeg, .jpg, .png`),
-        false
-      );
-    }
-  },
-}).single("profile-picture");
+import { getSignedImageUrl } from "../services/aws/s3.mjs";
+import { findUserProfile, updateDisplayName } from "../models/user.model.mjs";
 
 export const fetchUserProfile = async (req, res) => {
   try {
@@ -35,14 +11,11 @@ export const fetchUserProfile = async (req, res) => {
   }
 };
 
-export const updateProfile = (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-
-    await uploadImage(req.user.id, req.file.buffer, req.file.mimetype);
-
-    res.status(200).json({ message: "File uploaded successfully" });
-  });
+export const updateProfile = async (req, res) => {
+  try {
+    await updateDisplayName(req.user.id, req.body.displayName);
+    res.status(200);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
