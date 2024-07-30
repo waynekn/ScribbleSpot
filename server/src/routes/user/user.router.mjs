3c8ad5722d30dd4f74ce1983c78127a6da526b9f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-30 22:53:59 +0300

    import multer middleware

--- server/src/routes/user/user.router.mjs
+++ server/src/routes/user/user.router.mjs
@@ -4,10 +4,11 @@ import {
   fetchUserProfile,
   updateProfile,
 } from "../../controllers/user.controller.mjs";
+import { getImageBuffer } from "../../middleware/multer/multer.mjs";
 
 const userRouter = express.Router();
 
 userRouter.get("/profile", fetchUserProfile);
-userRouter.post("/profile", updateProfile);
+userRouter.post("/profile", getImageBuffer, updateProfile);
 
 export default userRouter;
