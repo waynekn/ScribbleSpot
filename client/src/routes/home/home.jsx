ae0bb433ae7e9dce9e895839f76eda99f20b10ad
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-08 02:40:33 +0300

    add an effect to correct persisted state mismatch bug

--- client/src/routes/home/home.jsx
+++ client/src/routes/home/home.jsx
@@ -1,7 +1,26 @@
-const Home = () => (
-  <>
-    <h1>Welcome to ScribbleSpot</h1>
-    <p> Explore in Beta</p>
-  </>
-);
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { clearCurrentUser } from "../../store/user/user.slice";
+import { checkAuthStatus } from "../../api-requests/requests";
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getAuthStatus = async () => {
+      try {
+        await checkAuthStatus();
+      } catch (error) {
+        dispatch(clearCurrentUser());
+      }
+    };
+    getAuthStatus();
+  });
+
+  return (
+    <>
+      <h1>Welcome to ScribbleSpot</h1>
+      <p> Explore in Beta</p>
+    </>
+  );
+};
 export default Home;
