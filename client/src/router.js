41d67a8bcb9eb8f6f380d815a93d56ed2cb91482
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-09 23:23:14 +0300

    add profile route

--- client/src/router.js
+++ client/src/router.js
@@ -8,6 +8,7 @@ import SignInForm from "./components/sign-in-page/sign-in-form.component";
 import SignUpForm from "./components/sign-up-page/sign-up-form.component";
 import Editor from "./components/editor/editor.component";
 import SettingsForm from "./components/settings-form/settings-form.component";
+import ProfilePage from "./components/profile/profile.component";
 import Home from "./routes/home/home";
 
 export const router = createBrowserRouter([
@@ -35,6 +36,11 @@ export const router = createBrowserRouter([
   {
     path: "stories",
     element: <Article />,
+  },
+  {
+    path: "profile",
+    element: <ProfilePage />,
+    children: [{ path: "settings", element: <SettingsForm /> }],
   },
   { path: "editor", element: <Editor /> },
   {
