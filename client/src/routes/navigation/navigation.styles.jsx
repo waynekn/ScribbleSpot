8300c59f059e859b9756c915fb4f6c514d5c221d
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-10 19:51:33 +0300

    style nav bar

---
+++ client/src/routes/navigation/navigation.styles.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Nav = styled.nav`
+  margin: 5px 10px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const NavLink = styled(Link)`
+  margin: 0 5px;
+`;
+
+// .navbar {
+//   margin: 5px 10px;
+//   display: flex;
+//   justify-content: flex-end;
+// }
+
+// .log-in-icon {
+//   margin-right: 5px;
+// }
+
+// .nav-link {
+//   margin: 0 5px;
+// }
