d108670158b5c2b00dcf425da1e967042df60c49
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-13 18:37:11 +0300

    delete files

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { router } from "./router";
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
-export default App;
