86ab3d0676067bad3ddf98f42f023076520b9d38
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-15 19:47:15 +0300

    rename file

--- client/src/components/article/article.component.jsx
+++ client/src/components/article/article.component.jsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.bubble.css";
-
 const Article = () => {
-  const [editorValue, setEditorValue] = useState({});
-  console.log(editorValue);
-
-  return (
-    <div>
-      <ReactQuill value={editorValue} readOnly={true} theme="bubble" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Article;
