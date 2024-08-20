8224627f4a71c7489a0183e7d4fb0a57e6f9ec37
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-20 18:36:09 +0300

    create blog post slice

---
+++ client/src/store/blog/blog-post.slice.ts
@@ -0,0 +1,72 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { uploadContent } from "../../api-requests/requests";
+
+type BlogState = {
+  notification: string | null;
+  isLoading: boolean;
+};
+
+const initialState: BlogState = {
+  notification: null,
+  isLoading: false,
+};
+
+type Doc = {
+  title: string;
+  htmlContent: string;
+};
+
+export const postBlog = createAsyncThunk(
+  "blog/postBlog",
+  async (doc: Doc, { rejectWithValue }) => {
+    try {
+      const title = doc.title;
+      const htmlContent = doc.htmlContent;
+      const response = await uploadContent(title, htmlContent);
+      return response.message;
+    } catch (error) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue("An unknown error occurred.");
+    }
+  }
+);
+
+const blogSlice = createSlice({
+  name: "blog",
+  initialState,
+  reducers: {
+    setNotificationMessage(state: BlogState, action: PayloadAction<string>) {
+      state.notification = action.payload;
+    },
+    clearNotificationMessage(state: BlogState) {
+      state.notification = null;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(postBlog.pending, (state: BlogState) => {
+        state.isLoading = true;
+      })
+      .addCase(
+        postBlog.fulfilled,
+        (state: BlogState, action: PayloadAction<string>) => {
+          (state.isLoading = false), (state.notification = action.payload);
+        }
+      )
+      .addCase(postBlog.rejected, (state: BlogState, action) => {
+        state.isLoading = false;
+        if (typeof action.payload === "string") {
+          state.notification = action.payload;
+        } else {
+          state.notification = "An error occurred";
+        }
+      });
+  },
+});
+
+export const { setNotificationMessage, clearNotificationMessage } =
+  blogSlice.actions;
+
+export const blogReducer = blogSlice.reducer;
