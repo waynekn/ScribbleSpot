7a0ee2dc8e2a2a2b8c6a5b19c798f6e207062d24
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-20 18:27:26 +0300

    create posts model

---
+++ server/src/models/posts/blog.model.mjs
@@ -0,0 +1,30 @@
+import posts from "../../schemas/post/post.schema.mjs";
+
+export const uploadBlog = async (authorId, title, htmlContent) => {
+  try {
+    const authorPost = await posts.findOne({ authorId });
+
+    if (authorPost) {
+      const existingBlog = authorPost.blogs.find(
+        (blog) => blog.title === title
+      );
+
+      if (existingBlog) {
+        throw new Error("Can't have posts with the same title");
+      }
+
+      authorPost.blogs.push({ title, content: htmlContent });
+      await authorPost.save();
+    } else {
+      const newPost = new posts({
+        authorId,
+        blogs: [{ title, content: htmlContent }],
+      });
+
+      await newPost.save();
+    }
+  } catch (error) {
+    console.error("Error uploading blog:", error);
+    throw new Error("Error uploading blog");
+  }
+};
