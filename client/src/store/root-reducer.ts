bfa15183001e9ab6968d77e74880076d3ed74cb7
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-20 18:40:40 +0300

    import blog reducer

--- client/src/store/root-reducer.ts
+++ client/src/store/root-reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/user.slice";
+import { blogReducer } from "./blog/blog-post.slice";
 
 export const rootReducer = combineReducers({
   user: userReducer,
+  postBlog: blogReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
