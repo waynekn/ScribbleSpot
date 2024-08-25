2b001a1741e39290dd48f55501cbe2dc50a2b804
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-26 00:28:51 +0300

    create route for getting blog content

--- server/src/routes/posts/posts.router.js
+++ server/src/routes/posts/posts.router.js
@@ -3,8 +3,10 @@ import { authenticateJWT } from "../../middleware/auth/jwt.auth.js";
 import {
   postBlog,
   getBlogTitles,
+  getBlogContent,
 } from "../../controllers/posts/blog.controller.js";
 export const postsRouter = express.Router();
 
 postsRouter.post("/blog", authenticateJWT, postBlog);
 postsRouter.get("/titles", authenticateJWT, getBlogTitles);
+postsRouter.post("/content", authenticateJWT, getBlogContent);
