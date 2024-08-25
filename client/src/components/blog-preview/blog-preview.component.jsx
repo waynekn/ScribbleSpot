216cb1a6d6bca2001d79a7b8c00d0e7494a9f913
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-26 00:25:51 +0300

    create blog preview component

---
+++ client/src/components/blog-preview/blog-preview.component.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setNotificationMessage } from "../../store/blog/blog-post.slice";
+import { Outlet } from "react-router-dom";
+import { fetchTitles } from "../../api-requests/requests";
+import { BlogLink } from "./blog-preview.styles";
+const BlogPreview = () => {
+  const [blogTitles, setBlogTitles] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getTitles = async () => {
+      try {
+        const { titles } = await fetchTitles();
+        setBlogTitles(titles);
+      } catch (error) {
+        dispatch(setNotificationMessage(error.message));
+      }
+    };
+    getTitles();
+  }, [dispatch]);
+
+  return (
+    <div>
+      {blogTitles.map((blogPost) => (
+        <BlogLink
+          key={blogPost.title}
+          to={blogPost.title}
+          target="_parent"
+          className="blog-link"
+        >
+          {blogPost.title}
+        </BlogLink>
+      ))}
+      <Outlet />
+    </div>
+  );
+};
+
+export default BlogPreview;
