809c9f3ae67db6c6740d5fdaaa3fd729699a5759
Wayne Kinyanjui <waynekn24@gmail.com>
2024-08-27 19:26:39 +0300

    style blog component

---
+++ client/src/components/blog/blog.styles.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const BlogContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`;
+
+export const DisplayName = styled.p`
+  color: rgba(128, 128, 128, 0.603);
+  text-align: right;
+  font-style: italic;
+`;
+
+export const BlogTitle = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const BlogContent = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+`;
