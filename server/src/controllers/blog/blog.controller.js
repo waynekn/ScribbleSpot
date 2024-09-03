ba0424a6cd433b224ecbe662c3f0a90d370d9183
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-03 19:59:49 +0300

    add function to handle blog delete requests

--- server/src/controllers/blog/blog.controller.js
+++ server/src/controllers/blog/blog.controller.js
@@ -3,6 +3,7 @@ import {
   uploadBlog,
   fetchBlogContent,
   fetchBlogTitles,
+  deleteBlog,
 } from "../../models/blog/blog.model.js";
 import { fetchUserProfile } from "../../models/user/user.model.js";
 
@@ -53,5 +54,22 @@ export const getBlogContent = async (req, res) => {
     res.status(200).json({ blog });
   } catch (error) {
     res.status(500).json({ error: "A server errror occured" });
+  }
+};
+
+export const handleDelete = async (req, res) => {
+  try {
+    const authorId = req.user.id;
+    const title = req.body.title;
+    const existingTitle = await checkExistingTitle(authorId, title);
+
+    if (!existingTitle) {
+      return res.status(400).json({ error: "Could not find blog" });
+    }
+
+    await deleteBlog(authorId, title);
+    return res.status(200).json({ message: "Blog successfully deleted" });
+  } catch (error) {
+    return res.status(500).json({ error: "Could not delete blog" });
   }
 };
