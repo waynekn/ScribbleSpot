bbd48ea676992fa548212d32e2c582aec14cff37
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-03 20:05:06 +0300

    add route to delete blog

--- server/src/routes/blog/blog.router.js
+++ server/src/routes/blog/blog.router.js
@@ -3,9 +3,11 @@ import {
   postBlog,
   getBlogTitles,
   getBlogContent,
+  handleDelete,
 } from "../../controllers/blog/blog.controller.js";
 export const blogRouter = express.Router();
 
 blogRouter.post("/blog", postBlog);
 blogRouter.get("/titles", getBlogTitles);
 blogRouter.post("/content", getBlogContent);
+blogRouter.post("/blog/delete", handleDelete);
