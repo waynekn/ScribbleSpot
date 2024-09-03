909200b6f0e486e0e6fecaa1b85daca7c789e0dc
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-03 19:11:47 +0300

    stop displaying time

--- client/src/components/toast/toast.component.jsx
+++ client/src/components/toast/toast.component.jsx
@@ -4,20 +4,9 @@ import { ToastBottomRight } from "./toast.styles";
 
 const MessageToast = ({ message }) => {
   const [show, setShow] = useState(false);
-  const [displayTime, setDisplayTime] = useState("");
   const toggleShow = () => setShow(!show);
 
-  const getTime = () => {
-    const date = new Date(Date.now());
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-
-    return `${hours}:${minutes}:${seconds} hrs`;
-  };
-
   useEffect(() => {
-    setDisplayTime(getTime());
     setShow(true);
   }, [message]);
 
@@ -30,7 +19,6 @@ const MessageToast = ({ message }) => {
     >
       <ToastBottomRight.Header>
         <strong className="me-auto">ScribbleSpot</strong>
-        <small>{displayTime}</small>
       </ToastBottomRight.Header>
       <ToastBottomRight.Body>{message}</ToastBottomRight.Body>
     </ToastBottomRight>
