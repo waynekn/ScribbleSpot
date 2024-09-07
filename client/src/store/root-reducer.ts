e0c98d1540b4b645ef6b675573e0983aa17a584c
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-07 21:57:08 +0300

    add profile to combined reducers

--- client/src/store/root-reducer.ts
+++ client/src/store/root-reducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/user.slice";
 import { blogReducer } from "./blog/blog-post.slice";
+import { profileReducer } from "./profile/profile.slice";
 
 export const rootReducer = combineReducers({
   user: userReducer,
   postBlog: blogReducer,
+  profile: profileReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
