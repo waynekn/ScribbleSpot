785a43153b007ad4bd6ea08b480f098d134809d0
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-07 21:59:59 +0300

    create profile slice

---
+++ client/src/store/profile/profile.slice.ts
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { getUserProfile } from "../../api-requests/requests";
+import { User } from "../../api-requests/requests";
+
+type ProfileState = User & {
+  isLoading: boolean;
+};
+
+export const fetchProfile = createAsyncThunk(
+  "profile/fetchProfile",
+  async (userName: string, { rejectWithValue }) => {
+    try {
+      const { profile } = await getUserProfile(userName);
+      return profile;
+    } catch (error) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue("An unknown error occured");
+    }
+  }
+);
+
+const initialState: ProfileState = {
+  email: "",
+  userName: "",
+  profilePicture: "",
+  dateJoined: new Date(),
+  isLoading: false,
+};
+
+const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfile.pending, (state: ProfileState) => {
+        state.isLoading = true;
+      })
+      .addCase(
+        fetchProfile.fulfilled,
+        (state: ProfileState, action: PayloadAction<User>) => {
+          const newProfileState: ProfileState = {
+            ...action.payload,
+            isLoading: false,
+          };
+          return newProfileState;
+        }
+      )
+      .addCase(fetchProfile.rejected, (state: ProfileState, action) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export const profileReducer = profileSlice.reducer;
