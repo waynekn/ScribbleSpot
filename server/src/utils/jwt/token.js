af88a7c9aad73ff9ec4d8ee16af4600c5c29faec
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-07 17:11:50 +0300

    add username to token payload

--- server/src/utils/jwt/token.js
+++ server/src/utils/jwt/token.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const generateToken = (user) => {
   const payload = {
     id: user._id,
+    userName: user.userName,
   };
   return jwt.sign(payload, process.env.JWT_SECRET);
 };
