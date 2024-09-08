f0365efb1600cd5af182654a56e14b1af405f3da
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-08 17:32:53 +0300

    use profile state to get blog content

--- client/src/components/blog/blog.component.jsx
+++ client/src/components/blog/blog.component.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Color from "@tiptap/extension-color";
@@ -11,12 +11,15 @@ import Superscript from "@tiptap/extension-superscript";
 import { fetchBlogContent } from "../../api-requests/requests";
 import { setNotificationMessage } from "../../store/blog/blog-post.slice";
 import { BlogContainer, UserName, BlogTitle, BlogContent } from "./blog.styles";
+
+import { selectProfile } from "../../store/profile/profile.selector";
 import "../editor/editor.styles.scss";
 
 const Blog = () => {
   const { titleSlug } = useParams();
   const [blog, setBlog] = useState({});
   const dispatch = useDispatch();
+  const profile = useSelector(selectProfile);
 
   const editor = useEditor({
     extensions: [
@@ -34,14 +37,14 @@ const Blog = () => {
   useEffect(() => {
     const getBlog = async () => {
       try {
-        const { blog } = await fetchBlogContent(titleSlug);
+        const { blog } = await fetchBlogContent(titleSlug, profile.userName);
         setBlog(blog);
       } catch (error) {
         dispatch(setNotificationMessage(error.message));
       }
     };
     getBlog();
-  }, [titleSlug, dispatch]);
+  }, [titleSlug, dispatch, profile]);
 
   useEffect(() => {
     if (editor && blog.content) {
