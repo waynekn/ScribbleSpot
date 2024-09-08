2b99a474a851786ab6a66ec66abd467a053a11e9
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-08 17:20:07 +0300

    add and modify styles to accomodate integration of search bar

--- client/src/routes/navigation/navigation.styles.jsx
+++ client/src/routes/navigation/navigation.styles.jsx
@@ -4,9 +4,21 @@ import { Link } from "react-router-dom";
 export const Nav = styled.nav`
   margin: 5px 10px;
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const NavLink = styled(Link)`
   margin: 0 5px;
+`;
+
+export const SearchWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 5px;
 `;
