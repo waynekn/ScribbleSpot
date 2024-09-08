b9f9951d467c36042ebfa562e3180be1f3acdbd0
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-08 17:20:33 +0300

    integrate search bar

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -3,29 +3,40 @@ import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import Button from "react-bootstrap/Button";
 import DropDown from "../../components/dropdown/dropdown.component";
+import SearchBar from "../../components/search-bar/search-bar.component";
 
-import { Nav, NavLink } from "./navigation.styles";
+import {
+  Nav,
+  NavLink,
+  SearchWrapper,
+  ButtonWrapper,
+} from "./navigation.styles";
 const NavBar = () => {
   const currentUser = useSelector(selectCurrentUser);
   return (
     <>
       <Nav>
-        <NavLink to="about-us">
-          <Button variant="light">About us</Button>
-        </NavLink>
-        <NavLink to="stories">
-          <Button variant="light">Read</Button>
-        </NavLink>
-        <NavLink to="editor">
-          <Button variant="light">Write</Button>
-        </NavLink>
-        {currentUser.isLoggedIn ? (
-          <DropDown />
-        ) : (
-          <NavLink to="authentication/sign-in">
-            <Button variant="dark">Sign in</Button>
+        <SearchWrapper>
+          <SearchBar />
+        </SearchWrapper>
+        <ButtonWrapper>
+          <NavLink to="about-us">
+            <Button variant="light">About us</Button>
           </NavLink>
-        )}
+          <NavLink to="stories">
+            <Button variant="light">Read</Button>
+          </NavLink>
+          <NavLink to="editor">
+            <Button variant="light">Write</Button>
+          </NavLink>
+          {currentUser.isLoggedIn ? (
+            <DropDown />
+          ) : (
+            <NavLink to="authentication/sign-in">
+              <Button variant="dark">Sign in</Button>
+            </NavLink>
+          )}
+        </ButtonWrapper>
       </Nav>
       <Outlet />
     </>
