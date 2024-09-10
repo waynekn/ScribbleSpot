4a8e2a052d3ea3c65e57dd610ea4a08e1e2e38d1
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-10 12:24:35 +0300

    document reason for making /profile handle all requests

--- server/src/routes/user/user.router.js
+++ server/src/routes/user/user.router.js
@@ -9,6 +9,11 @@ import { processForm } from "../../middleware/multer/multer.js";
 
 const userRouter = express.Router();
 
+/**
+ * Handle all requests to the /profile endpoint.
+ * After successful authentication, the client does not have a username available for POST requests.
+ * The username is retrieved from the cookie issued during authentication.
+ */
 userRouter.all("/profile", getUserProfile);
 userRouter.post("/profile/update", processForm, updateProfile);
 userRouter.post("/profile/profile-picture", getImageUrl);
