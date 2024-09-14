70f70a54af70944b81ec7c8c5a9164363a528d09
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-14 19:13:45 +0300

    use popover form to get auth credentials for local sign up requests

--- client/src/components/sign-up/sign-up-form.component.jsx
+++ client/src/components/sign-up/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { authenticateGoogleUser } from "../../api-requests/requests";
+import PopoverForm from "../popover-form/popover-form.component";
 import {
   SignUpPage,
   SignUpButtonsContainer,
@@ -8,6 +10,16 @@ import {
   ProviderButton,
 } from "./sign-up-form.styles";
 const SignUpForm = () => {
+  const [showPopoverForm, setShowPopoverForm] = useState(false);
+
+  const handleEmailButtonClick = () => {
+    setShowPopoverForm(true);
+  };
+
+  const handleClosePopover = () => {
+    setShowPopoverForm(false);
+  };
+
   return (
     <SignUpPage>
       <SignUpButtonsContainer>
@@ -16,13 +28,22 @@ const SignUpForm = () => {
           <ProviderButton onClick={() => authenticateGoogleUser("signup")}>
             Google
           </ProviderButton>
-          <ProviderButton>Email</ProviderButton>
+          <ProviderButton onClick={handleEmailButtonClick}>
+            Email
+          </ProviderButton>
         </ProviderButtonsContainer>
         <p>
           Already have an account?
           <Link to="../authentication/sign-in">Sign in</Link>
         </p>
       </SignUpButtonsContainer>
+      {showPopoverForm && (
+        <PopoverForm
+          onClose={handleClosePopover}
+          action="Sign up"
+          provider={"Email"}
+        />
+      )}
     </SignUpPage>
   );
 };
