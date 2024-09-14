deaf5812394084020b292bab2180a6201440809f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-14 19:16:22 +0300

    add password to schema

--- server/src/schemas/user/user.schema.js
+++ server/src/schemas/user/user.schema.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
+//oauth users dont have a password so the password
+//key is not required
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   userName: { type: String, required: true, unique: true },
   profilePicture: { type: String, required: true },
   dateJoined: { type: Date, required: true },
+  password: String,
 });
 
 export default mongoose.model("User", userSchema);
