58adfeecefbb4028e3d9a7d1daa75a9a15025a6e
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-14 19:07:07 +0300

    create thunk to handle authentication for local users

--- client/src/store/user/user.slice.ts
+++ client/src/store/user/user.slice.ts
@@ -1,12 +1,30 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import { getUserProfile } from "../../api-requests/requests";
-import { User } from "../../api-requests/requests";
+import {
+  getUserProfile,
+  User,
+  sendLocalAuthRequest,
+} from "../../api-requests/requests";
 
 export type CurrentUser = User & {
   isLoggedIn: boolean;
   isLoading: boolean;
   notificationMessage: string | null;
 };
+
+type SignInUser = {
+  emailOrUsername: string;
+  password: string;
+  action: "signin";
+};
+
+type SignUpUser = {
+  email: string;
+  userName: string;
+  password: string;
+  action: "signup";
+};
+
+export type AuthCredentials = SignInUser | SignUpUser;
 
 const initialState: CurrentUser = {
   email: "",
@@ -41,6 +59,26 @@ export const fetchCurrentUser = createAsyncThunk(
   }
 );
 
+/*
+ * Local is used technically as in not using other oauth providers
+ * Used to sign in user of type SignInUser or
+ * sign up a user of type SignUpUser
+ */
+export const authenticateLocalUser = createAsyncThunk(
+  "user/authenticateLocalUser",
+  async (user: AuthCredentials, { rejectWithValue }) => {
+    try {
+      const { profile } = await sendLocalAuthRequest(user);
+      return profile;
+    } catch (error) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -60,6 +98,9 @@ const userSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      /**
+       * fetchCurrentUser thunk
+       */
       .addCase(fetchCurrentUser.pending, (state: CurrentUser) => {
         state.isLoading = true;
         state.notificationMessage = null;
@@ -77,6 +118,34 @@ const userSlice = createSlice({
       )
       .addCase(fetchCurrentUser.rejected, (state: CurrentUser, action) => {
         state.isLoading = false;
+        if (typeof action.payload === "string") {
+          state.notificationMessage = action.payload;
+        } else {
+          state.notificationMessage = "An unknown error occurred";
+        }
+      })
+      /**
+       *authenticateLocalUser thunk
+       */
+      .addCase(authenticateLocalUser.pending, (state: CurrentUser) => {
+        state.isLoading = true;
+        state.notificationMessage = null;
+      })
+      .addCase(
+        authenticateLocalUser.fulfilled,
+        (state: CurrentUser, action: PayloadAction<User>) => {
+          const newUser: CurrentUser = {
+            ...action.payload,
+            isLoading: false,
+            isLoggedIn: true,
+            notificationMessage: null,
+          };
+          return newUser;
+        }
+      )
+      .addCase(authenticateLocalUser.rejected, (state: CurrentUser, action) => {
+        state.isLoading = false;
+        state.isLoggedIn = false;
         if (typeof action.payload === "string") {
           state.notificationMessage = action.payload;
         } else {
