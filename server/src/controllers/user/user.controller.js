e22a39f6b2cc5d40b6460959e15d0a42f6ea00a4
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-17 00:24:15 +0300

    document user controller

--- server/src/controllers/user/user.controller.js
+++ server/src/controllers/user/user.controller.js
@@ -5,6 +5,24 @@ import {
 } from "../../models/user/user.model.js";
 import { getCache, updateCache } from "../../models/cache/url-cache.model.js";
 
+/**
+ * Handles the request to get a user profile.
+ * This function retrieves the profile of a user based on the username provided.
+ * For OAuth users authenticated via Google, the username is initially retrieved
+ * from the authenticated user object in the request. Subsequent requests should
+ * include the username in the request body.
+ *
+ * @param {Object} req - The request object
+ * @param {Object} req.user - The authenticated user object
+ * @param {String} req.user.id - The ID of the authenticated user.
+ * @param {String} req.user.userName - The username for which to fetch the profile.
+ * @param {Object} req.body - The request body
+ * @param {String} req.body.userName - The username of the request profile
+ * @param {*} res
+ * @returns {Object} res.status(200) - If profile is succesfully retreived.
+ * @returns {Object} res.status(404) - If the profile is not found.
+ * @returns {Object} res.status(500) - If an error occurred.
+ */
 export const getUserProfile = async (req, res) => {
   try {
     const userName = req.body?.userName || req.user?.userName;
@@ -19,6 +37,25 @@ export const getUserProfile = async (req, res) => {
   }
 };
 
+/**
+ * Handles requests to update a users profile
+ *
+ * If a file is uploaded, it is used to update the user's profile picture.
+ * If a new username is provided, it is updated accordingly.
+ *
+ * @param {Object} req - The request object
+ * @param {Object} req.user - The authenticated user object
+ * @param {String} req.user.id - The ID of the authenticated user.
+ * @param {String} req.user.userName - The username of the authenticated user.
+ * @param {Object} req.body - The request body.
+ * @param {String} req.body.userName - The username the user intends to switch to.
+ * @param {Object} req.file - The request file object, added by multer.
+ * @param {String} req.file.buffer - The buffer of the uploaded image file.
+ * @param {String} req.file.mimetype - The MIME type of the uploaded image file.
+ * @param {*} res
+ * @returns res.status(200) - for a successfull update.
+ * @returns res.status(500) - if an error occurred during the update process.
+ */
 export const updateProfile = async (req, res) => {
   try {
     if (req.file) {
@@ -34,6 +71,26 @@ export const updateProfile = async (req, res) => {
     });
   }
 };
+
+/**
+ * Handles requests to get a signed URL for displaying a user's profile picture.
+ * This function fetches a signed URL from S3 that can be used as the `src` attribute
+ * of an `<img>` tag to display the profile picture.
+ *
+ * The function first checks a cache to see if a valid signed URL is available.
+ * If not, it retrieves a new signed URL from S3, updates the cache, and then returns the URL.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.user - The authenticated user object.
+ * @param {String} req.user.id - The ID of the authenticated user.
+ * @param {String} req.user.userName - The username of the authenticated user.
+ * @param {Object} req.body - The request body.
+ * @param {String} req.body.userName - The username of the user whose profile picture is being fetched.
+ * @param {String} req.body.imageKey - The key of the user's profile picture in the AWS S3 bucket.
+ * @param {*} res - The response object.
+ * @returns {Object} res.status(200) - If the signed URL was retrieved successfully.
+ * @returns {Object} res.status(500) - If an error occurred.
+ */
 
 export const getImageUrl = async (req, res) => {
   const imageKey = req.body.imageKey;
