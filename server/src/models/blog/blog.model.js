7191cd7cb6d7a4f732810ac92e1a4876ac0e343c
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-18 21:31:30 +0300

    refactor: return object id when fetching a blog

--- server/src/models/blog/blog.model.js
+++ server/src/models/blog/blog.model.js
@@ -66,7 +66,7 @@ export const fetchBlogTitles = async (userName) => {
 export const fetchBlogContent = async (userName, titleSlug) => {
   const posts = await blogs.findOne(
     { userName, titleSlug },
-    { _id: 0, title: 1, content: 1, userName: 1, datePosted: 1 }
+    { title: 1, content: 1, userName: 1, datePosted: 1 }
   );
   return posts;
 };
