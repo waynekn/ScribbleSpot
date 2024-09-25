1cf453ed33c934950b70099d6007308669603a8d
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-25 19:47:58 +0300

    use renamed blog slice

--- client/src/store/root-reducer.ts
+++ client/src/store/root-reducer.ts
@@ -1,12 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/user.slice";
-import { blogReducer } from "./blog/blog-post.slice";
+import { blogReducer } from "./blog/blog.slice";
 import { profileReducer } from "./profile/profile.slice";
 
 export const rootReducer = combineReducers({
   user: userReducer,
-  postBlog: blogReducer,
+  blog: blogReducer,
   profile: profileReducer,
 });
 
