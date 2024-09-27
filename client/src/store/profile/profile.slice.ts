f296b3ea9adfb8b183d6ff32d0747b2a3fc7c331
Wayne Kinyanjui <waynekn24@gmail.com>
2024-09-27 15:34:21 +0300

    refactor: navigate the user to authenticate if not authenticated

--- client/src/store/profile/profile.slice.ts
+++ client/src/store/profile/profile.slice.ts
@@ -1,19 +1,31 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import { getUserProfile } from "../../api-requests/requests";
-import { User } from "../../api-requests/requests";
+import { getUserProfile, User } from "../../api-requests/requests";
+import { AuthError } from "../../api-requests/request-errors/errors";
 
 type ProfileState = User & {
   isLoading: boolean;
 };
 
+/**
+ * The value of the navigate key is the function returned from react-router-dom's
+ * useNavigate() hook. It's used to navigate the user to authenticate themselves if
+ * they are not authenticated i.e; when an AuthError occurs.
+ */
+type FetchUserProfilePayload = {
+  userName: string;
+  navigate: Function;
+};
+
 export const fetchUserProfile = createAsyncThunk(
   "profile/fetchUserProfile",
-  async (userName: string, { rejectWithValue }) => {
+  async (payload: FetchUserProfilePayload, { rejectWithValue }) => {
     try {
-      const { profile } = await getUserProfile(userName);
+      const { profile } = await getUserProfile(payload.userName);
       return profile;
     } catch (error) {
-      if (error instanceof Error) {
+      if (error instanceof AuthError) {
+        payload.navigate();
+      } else if (error instanceof Error) {
         return rejectWithValue(error.message);
       }
       return rejectWithValue("An unknown error occured");
