8efa6093e0793a6465cb5e9df9d493abd8d105af
Wayne Kinyanjui <waynekn24@gmail.com>
2024-10-01 16:25:20 +0300

    add route for user search functionality

--- server/src/routes/user/user.router.js
+++ server/src/routes/user/user.router.js
@@ -4,11 +4,13 @@ import {
   getUserProfile,
   updateProfile,
   getImageUrl,
+  getUserNameSuggestions,
 } from "../../controllers/user/user.controller.js";
 import { processForm } from "../../middleware/multer/multer.js";
 
 const userRouter = express.Router();
 
+userRouter.post("/", getUserNameSuggestions);
 /**
  * Handle all requests to the /profile endpoint.
  * After successful authentication, the client does not have a username available for POST requests.
