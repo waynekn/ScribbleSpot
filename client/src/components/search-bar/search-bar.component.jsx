aed996d17fa7e16410e8ae545ab2c957d2b0cc6d
Wayne Kinyanjui <waynekn24@gmail.com>
2024-10-04 14:39:13 +0300

    modify searchbar to handle searching for blogs and users

--- client/src/components/search-bar/search-bar.component.jsx
+++ client/src/components/search-bar/search-bar.component.jsx
@@ -1,33 +1,50 @@
 import { useState, useRef } from "react";
-import { getUserSuggestions } from "../../api-requests/requests";
+import {
+  getUserSuggestions,
+  getBlogSuggestions,
+} from "../../api-requests/requests";
 
 import {
   SearchContainer,
   SearchInput,
+  SuggestionCategory,
   SuggestionsList,
   SuggestionItem,
   SuggestionLink,
 } from "./search-bar.styles";
 
 const SearchBar = () => {
-  const [searchProfile, setSearchProfile] = useState("");
+  const [searchSearchBarValue, setSearchBarValue] = useState("");
   const [suggestedUsers, setSuggestedUsers] = useState([]);
+  const [suggestedBlogs, setSuggestedBlogs] = useState([]);
   const hasInputText = useRef(false);
 
   const handleChange = async (e) => {
-    const userName = e.target.value;
-    setSearchProfile(userName);
+    const searchString = e.target.value;
+    setSearchBarValue(searchString);
+    const trimmedSearchString = searchString.trim();
 
-    if (userName) {
+    const hasSearchString = searchString.replace(/\s/g, "");
+
+    if (hasSearchString) {
       hasInputText.current = true;
-      const suggestedUsers = await getUserSuggestions(userName);
+
+      const usersPromise = getUserSuggestions(trimmedSearchString);
+      const blogsPromise = getBlogSuggestions(trimmedSearchString);
+
+      const [suggestedUsers, suggestedBlogs] = await Promise.all([
+        usersPromise,
+        blogsPromise,
+      ]);
 
       if (hasInputText.current) {
         setSuggestedUsers(suggestedUsers);
+        setSuggestedBlogs(suggestedBlogs);
       }
     } else {
       hasInputText.current = false;
       setSuggestedUsers([]);
+      setSuggestedBlogs([]);
     }
   };
 
@@ -36,19 +53,39 @@ const SearchBar = () => {
       <SearchInput
         type="text"
         placeholder="Search for a user"
-        value={searchProfile}
+        value={searchSearchBarValue}
         onChange={handleChange}
       />
+
       {suggestedUsers.length > 0 && (
-        <SuggestionsList>
-          {suggestedUsers.map((username, index) => (
-            <SuggestionItem key={index}>
-              <SuggestionLink to={`profile/${username}`}>
-                {username}
-              </SuggestionLink>
-            </SuggestionItem>
-          ))}
-        </SuggestionsList>
+        <div>
+          <SuggestionCategory>Users</SuggestionCategory>
+          <SuggestionsList>
+            {suggestedUsers.map((username, index) => (
+              <SuggestionItem key={index}>
+                <SuggestionLink to={`profile/${username}`}>
+                  {username}
+                </SuggestionLink>
+              </SuggestionItem>
+            ))}
+          </SuggestionsList>
+        </div>
+      )}
+      {suggestedBlogs.length > 0 && (
+        <div>
+          <SuggestionCategory>Blogs</SuggestionCategory>
+          <SuggestionsList>
+            {suggestedBlogs.map((blogSuggestion, index) => (
+              <SuggestionItem key={index}>
+                <SuggestionLink
+                  to={`blog/${blogSuggestion.titleSlug}/${blogSuggestion.id}`}
+                >
+                  {blogSuggestion.title}
+                </SuggestionLink>
+              </SuggestionItem>
+            ))}
+          </SuggestionsList>
+        </div>
       )}
     </SearchContainer>
   );
