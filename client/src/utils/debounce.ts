52c88936f856ab437ce5594e79cfa6362418b910
Wayne Kinyanjui <waynekn24@gmail.com>
2024-10-04 14:33:46 +0300

    debounce blog search

--- client/src/utils/debounce.ts
+++ client/src/utils/debounce.ts
@@ -1,4 +1,4 @@
-import { UserSuggestions } from "../api-requests/requests";
+import { UserSuggestions, Title } from "../api-requests/requests";
 
 export const debouncedGetUserSuggestions = (cb: Function, delay = 500) => {
   let timeout: ReturnType<typeof setTimeout>;
@@ -10,6 +10,21 @@ export const debouncedGetUserSuggestions = (cb: Function, delay = 500) => {
       timeout = setTimeout(async () => {
         const suggestions: UserSuggestions = await cb(args);
         resolve(suggestions);
+      }, delay);
+    });
+  };
+};
+
+export const debouncedGetBlogSuggestions = (cb: Function, delay = 500) => {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return (args: string) => {
+    clearTimeout(timeout);
+
+    return new Promise((resolve) => {
+      timeout = setTimeout(async () => {
+        const suggestedBlogs: Title = await cb(args);
+        resolve(suggestedBlogs);
       }, delay);
     });
   };
