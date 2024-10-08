016e1552355138deabfa66ae27a8072a759cb364
Wayne Kinyanjui <waynekn24@gmail.com>
2024-10-08 21:47:00 +0300

    remove unnecessary styles

--- client/src/routes/navigation/navigation.styles.jsx
+++ client/src/routes/navigation/navigation.styles.jsx
@@ -4,23 +4,10 @@ import { Link } from "react-router-dom";
 export const Nav = styled.nav`
   margin: 5px 10px;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
 `;
 
 export const NavLink = styled(Link)`
   margin: 0 5px;
-`;
-
-export const SearchWrapper = styled.div`
-  position: fixed;
-  top: 5px;
-  left: 40%;
-`;
-
-export const ButtonWrapper = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: flex-end;
-  gap: 5px;
 `;
