13ff24189293144d5ecf8a8c3714a21f3fd5af09
Wayne Kinyanjui <waynekn24@gmail.com>
2024-10-12 19:31:43 +0300

    fix ref to 'User' model

--- server/src/schemas/blog.schema.js
+++ server/src/schemas/blog.schema.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-  authorId: { type: String, required: true },
+  authorId: { type: String, required: true, ref: "User" },
   title: { type: String, required: true },
   titleSlug: { type: String, required: true },
   content: { type: String, required: true },
   datePosted: { type: Date, required: true },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }], // Array of user ObjectId's who liked
-  dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }], // Array of user ObjectId's who disliked
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Array of user ObjectId's who liked
+  dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Array of user ObjectId's who disliked
 });
 
 blogSchema.index({ authorId: 1, title: 1 }, { unique: true });
